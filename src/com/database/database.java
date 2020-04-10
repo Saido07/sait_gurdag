071a9ecc9bf48709a76760ad51180e20116db31a
sait_ <sait_@DESKTOP-A9KC9RP>
2020-04-10 18:02:29 +0300

    database hataları düzeltildi

--- src/com/database/database.java
+++ src/com/database/database.java
@@ -10,40 +10,35 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/**
- *
- * @author sait_
- */
+
 public class database {
 
-    /**
-     * @param args the command line arguments
-     * @throws java.sql.SQLException
-     */
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         
         Connection con = null;
-        Statement stmt = null;
         int result = 0;
         
         try {
             System.out.println("Connecting database...");
             Class.forName("org.hsqldb.jdbcDriver");
-            String url = "jdbc:hsqldb:file:D:\\Uni\\TMS-II\\II. Dönem\\Inf-202\\app_database\\; shutdown=true";
+            String url = "jdbc:hsqldb:file:C:\\Users\\sait_\\Desktop\\SEDB\\; shutdown=true";
             con = DriverManager.getConnection(url, "Sait" , "123456");
-            System.out.println("Database connected!");
             
+            Statement stmt = con.createStatement();
+            result = stmt.executeUpdate("CREATE TABLE Teste(Id INTEGER PRIMARY KEY)");
             
-            } catch (ClassNotFoundException e) {
+           
+            con.close();
+            System.out.println(result);
+        } catch (ClassNotFoundException e) {
             System.out.println("Database connection error:" + e);
-            } catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Database connection error:" + e);
-            }
+        }
         
         
       /*  try{
-            result = stmt.executeUpdate("CREATE TABLE Kullanici(id INTEGER PRIMARY KEY AUTOINCREMENT, isim VARCHAR(20) NOT NULL, soyisim VARCHAR(20) NOT NULL, imza-son-gecerlilik DATE, seviye int , PRIMARY KEY (id));");            
-        } catch(Exception e) {
+            } catch(Exception e) {
             e.printStackTrace(System.out);
         
         }*/
