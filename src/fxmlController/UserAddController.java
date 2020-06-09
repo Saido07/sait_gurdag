fbd58508e7e612724d6682b32f2b2fe5592f1621
sait_ <sait_@DESKTOP-A9KC9RP>
2020-06-09 11:22:12 +0300

    Kullanıcı ekleme ekranında hatalar ayrıştırıldı

--- src/fxmlController/UserAddController.java
+++ src/fxmlController/UserAddController.java
@@ -4,6 +4,10 @@ import com.BIN.Config;
 import com.BIN.User;
 import com.database.database;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
@@ -12,6 +16,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
 
 public class UserAddController extends AnchorPane {
 
@@ -33,6 +38,21 @@ public class UserAddController extends AnchorPane {
     private TextField level;
     @FXML
     private Label resultTxt;
+        
+    @FXML
+    private Label usernameL;
+
+    @FXML
+    private Label nameL;
+
+    @FXML
+    private Label surnameL;
+
+    @FXML
+    private Label levelL;
+
+    @FXML
+    private Label tarihL;
     
     database db = new database();
 
@@ -89,31 +109,100 @@ public class UserAddController extends AnchorPane {
         
         
         save.setOnAction(b -> {
-            if(User.getDb_id()==null){
-                addUser();
-            }else if(SelectUser.getValue().toString().equals("Yeni Kullanıcı")){                
-                addUser();
+            boolean na=false;
+            boolean su=false;
+            boolean us=false;
+            boolean ta=false;
+            boolean le=false;
+            boolean ta2=false;
+            
+            if(name.getText().isEmpty()==true){
+                nameL.setStyle("-fx-text-fill: red;");
+                name.setStyle("-fx-border-color: red");
+                na=false;
             }else{
-                if(User.getDb_id()!=null){
-                    try {
-                        result=db.doInBackground("updateUser", userName.getText().toString() , name.getText().toString(),
-                                surname.getText().toString(), level.getText().toString(), signature_e_date.getText().toString());
-                        if(result==false){
-                            resultTxt.setStyle("-fx-text-fill: red;");
-                            resultTxt.setText("Hatalı ya da Eksik Bilgi");
-                        }else{
-                            resultTxt.setStyle("-fx-text-fill: black;");
-                            resultTxt.setText("Kullanıcı Bilgileri Başarıyla Güncellendi");
-                            refreshSelectUser();
-                        }
-                    } catch (SQLException ex) {
-                        Logger.getLogger(UserAddController.class.getName()).log(Level.SEVERE, null, ex);
-                    } catch (ClassNotFoundException ex) {
-                        Logger.getLogger(UserAddController.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
+                nameL.setStyle("-fx-text-fill: black;");
+                name.setBorder(Border.EMPTY);
+                na=true;
+            }
+            if(userName.getText().isEmpty()==true){
+                usernameL.setStyle("-fx-text-fill: red;");
+                userName.setStyle("-fx-border-color: red");
+                us=false;
+            }else{
+                usernameL.setStyle("-fx-text-fill: black;");
+                userName.setBorder(Border.EMPTY);
+                us=true;
+            }
+            if(surname.getText().isEmpty()==true){
+                surnameL.setStyle("-fx-text-fill: red;");
+                surname.setStyle("-fx-border-color: red"); 
+                su=false;
+            }else{
+                surnameL.setStyle("-fx-text-fill: black;");
+                surname.setBorder(Border.EMPTY);
+                su=true;
+            }
+            if(level.getText().isEmpty()==true){
+                levelL.setStyle("-fx-text-fill: red;");
+                level.setStyle("-fx-border-color: red"); 
+                le=false;
+            }else{
+                levelL.setStyle("-fx-text-fill: black;");
+                level.setBorder(Border.EMPTY);
+                le=true;
+            }
+            if(signature_e_date.getText().isEmpty()==true){
+                signature_e_date.setStyle("-fx-border-color: red"); 
+                tarihL.setStyle("-fx-text-fill: red;");
+                ta=false;
+            }else{
+                signature_e_date.setBorder(Border.EMPTY);
+                tarihL.setStyle("-fx-text-fill: black;");
+                ta=true;
             }
             
+            Date date = null;
+            try {
+                date = new SimpleDateFormat("yyyy-MM-dd").parse(signature_e_date.getText().toString());
+                ta2=true;
+                signature_e_date.setBorder(Border.EMPTY);
+                tarihL.setStyle("-fx-text-fill: black;");
+            } catch (Exception ex) {
+                ta2=false;
+                signature_e_date.setStyle("-fx-border-color: red"); 
+                tarihL.setStyle("-fx-text-fill: red;");
+            }
+            
+            if(na==true && su==true && us==true && le==true && ta==true && ta2==true){
+                if(User.getDb_id()==null){
+                    addUser();
+                }else if(SelectUser.getValue().toString().equals("Yeni Kullanıcı")){                
+                    addUser();
+                }else{
+                    if(User.getDb_id()!=null){
+                        try {
+                            result=db.doInBackground("updateUser", userName.getText().toString() , name.getText().toString(),
+                                    surname.getText().toString(), level.getText().toString(), signature_e_date.getText().toString());
+                            if(result==false){
+                                resultTxt.setStyle("-fx-text-fill: red;");
+                                resultTxt.setText("Hatalı ya da Eksik Bilgi");
+                            }else{
+                                resultTxt.setStyle("-fx-text-fill: black;");
+                                resultTxt.setText("Kullanıcı Bilgileri Başarıyla Güncellendi");
+                                refreshSelectUser();
+                            }
+                        } catch (SQLException ex) {
+                            Logger.getLogger(UserAddController.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (ClassNotFoundException ex) {
+                            Logger.getLogger(UserAddController.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+            }else{
+                resultTxt.setStyle("-fx-text-fill: red;");
+                resultTxt.setText("Hatalı ya da Eksik Bilgi");
+            }
             
         });
         
@@ -173,8 +262,6 @@ public class UserAddController extends AnchorPane {
         SelectUser.getItems().setAll(User.getUsers());
         
     }
-    
-
-
+   
     
 }
